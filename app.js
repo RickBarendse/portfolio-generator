8a34f74576c55b396b2f2052ed4aa368facda490
Rick Barendse <dazedeyes@yahoo.com>
2022-03-27 15:39:32 -0700

    added project array to HTML

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const { truncate } = require('fs');
+// const { truncate } = require('fs');
+const fs = require('fs');
 const inquirer = require('inquirer');
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template.js');
 
 // const pageHTML = generatePage(name, github);
 
@@ -157,6 +156,12 @@ promptUser()
     //.then(answers => console.log(answers))
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
     });
+});
     //.then(projectAnswers => console.log(projectAnswers));
